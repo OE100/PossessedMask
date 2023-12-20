e094d0f84c0d3040ca86e4e70bb5824ad77732d7
OE.omergunr100 <omer.er10@gmail.com>
2023-12-20 04:39:53 +0200

    Fixed bug with dropping 2 handed item.
    Added config and made lag improvements.

--- PossessedMask/Patches/StartOfRoundPatch.cs
+++ PossessedMask/Patches/StartOfRoundPatch.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
+using BepInEx;
 using GameNetcodeStuff;
 using HarmonyLib;
 using UnityEngine;
@@ -11,11 +12,29 @@ namespace PossessedMask.Patches
     public class StartOfRoundPatch
     {
         private static PlayerControllerB localPlayer = null;
-        private static Dictionary<HauntedMaskItem, float> timeHeldByPlayer = new Dictionary<HauntedMaskItem, float>();
+        private static float timeHeldByPlayer = 0f;
         private static float nextTimeToSwitchSlot = 0f;
-
+        private static float timeUntilPossession = 0f;
+        
+        
         private static AudioClip[] possessionSounds = new AudioClip[3];
         private static AudioClip[] slotSwitchSounds = new AudioClip[2];
+
+        private static float minTimeToSwitchSlots;
+        private static float maxTimeToSwitchSlots;
+        private static float deltaTimeToSwitchSlots;
+        private static float minSwitchingSlotTime;
+        
+        private static float minTimeToPossess;
+        private static float maxTimeToPossess;
+        private static float deltaTimeToPossess;
+        private static float minPossessingTime;
+        
+        private static float minTimeToPossessPlayer;
+        private static float maxTimeToPossessPlayer;
+        private static float deltaTimeToPossessPlayer;
+        private static float maxPossessingPlayerTime;
+        
         
         [HarmonyPatch("Start")]
         [HarmonyPostfix]
@@ -34,6 +53,22 @@ namespace PossessedMask.Patches
             possessionSounds[2] = ab.LoadAsset<AudioClip>("possession3");
             slotSwitchSounds[0] = ab.LoadAsset<AudioClip>("slot1");
             slotSwitchSounds[1] = ab.LoadAsset<AudioClip>("slot2");
+            
+            // config loading
+            minTimeToSwitchSlots = Plugin.minTimeToSwitchSlots.Value;
+            maxTimeToSwitchSlots = Plugin.maxTimeToSwitchSlots.Value;
+            deltaTimeToSwitchSlots = Plugin.deltaTimeToSwitchSlots.Value;
+            minSwitchingSlotTime = Plugin.minSwitchingSlotTime.Value;
+            
+            minTimeToPossess = Plugin.minTimeToPossess.Value;
+            maxTimeToPossess = Plugin.maxTimeToPossess.Value;
+            deltaTimeToPossess = Plugin.deltaTimeToPossess.Value;
+            minPossessingTime = Plugin.minPossessingTime.Value;
+            
+            minTimeToPossessPlayer = Plugin.minTimeToPossessPlayer.Value;
+            maxTimeToPossessPlayer = Plugin.maxTimeToPossessPlayer.Value;
+            deltaTimeToPossessPlayer = Plugin.deltaTimeToPossessPlayer.Value;
+            maxPossessingPlayerTime = Plugin.maxPossessingPlayerTime.Value;
         }
         
         [HarmonyPatch("openingDoorsSequence")]
@@ -43,7 +78,8 @@ namespace PossessedMask.Patches
             // set local player
             localPlayer = __instance.localPlayerController;
             // clear dictionary
-            timeHeldByPlayer.Clear();
+            timeUntilPossession = Plugin.timeToStartPossession.Value;
+            timeHeldByPlayer = 0f;
             nextTimeToSwitchSlot = 0f;
             Plugin.Log.LogInfo("timeHeldByPlayer cleared and timers reset");
         }
@@ -63,8 +99,7 @@ namespace PossessedMask.Patches
             nextTimeToSwitchSlot -= Time.deltaTime;
             
             // Check player inventory for masks
-            int longestHeldIndex = -1;
-            float longestHeldTime = 0;
+            bool hasMask = false;
             
             for (int ind = 0; ind < localPlayer.ItemSlots.Length; ind++)
             {
@@ -72,66 +107,77 @@ namespace PossessedMask.Patches
                 if (currentItem == null ||
                     currentItem.GetType() != typeof(HauntedMaskItem)) continue;
                 // If found mask add to it's count and check if it's the longest in inventory or currently held
-                float time;
-                if (timeHeldByPlayer.ContainsKey((HauntedMaskItem)currentItem))
-                {
-                    time = timeHeldByPlayer[(HauntedMaskItem)currentItem] += Time.deltaTime;
-                }
-                else
-                {
-                    time = timeHeldByPlayer[(HauntedMaskItem)currentItem] = Time.deltaTime;
-                }
-
-                if (!(longestHeldTime < time)) continue;
-                
-                longestHeldIndex = ind;
-                longestHeldTime = time;
+                hasMask = true;
+                break;
+            }
+            if (hasMask)
+            {
+                timeHeldByPlayer += Time.deltaTime;
+                timeUntilPossession -= Time.deltaTime;
+            }
+            else
+            {
+                return;
             }
             // If a mask is the currently held item try to possess player
             var currentHeld = localPlayer.ItemSlots[localPlayer.currentItemSlot];
             if (currentHeld != null && 
                 currentHeld.GetType() == typeof(HauntedMaskItem))
             {
-                if (timeHeldByPlayer[(HauntedMaskItem)currentHeld] > 30f)
+                if (timeUntilPossession < 0f)
                 {
-                    float time = Random.Range(0.5f, 5f);
-                    Plugin.Log.LogInfo("Player has held mask for " + timeHeldByPlayer[(HauntedMaskItem)currentHeld] + " seconds, possessing for " + time + " seconds");
-                    timeHeldByPlayer[(HauntedMaskItem)currentHeld] = -time;
+                    float time = Random.Range(minTimeToPossessPlayer, maxTimeToPossessPlayer);
+                    minTimeToPossessPlayer += (minTimeToPossessPlayer < maxPossessingPlayerTime ? deltaTimeToPossessPlayer : 0f);
+                    maxTimeToPossessPlayer += (maxTimeToPossessPlayer < maxPossessingPlayerTime ? deltaTimeToPossessPlayer : 0f);
+                    
+                    Plugin.Log.LogInfo($"Player has held mask for enough time, possessing for {time} seconds");
+                    
+                    timeUntilPossession = Random.Range(minTimeToPossess, maxTimeToPossess);
+                    minTimeToPossess -= (minTimeToPossess > minPossessingTime ? deltaTimeToPossess : 0f);
+                    maxTimeToPossess -= (maxTimeToPossess > minPossessingTime ? deltaTimeToPossess : 0f);
+                    
                     IEnumerator possessPlayerDelayed = PossessPlayer((HauntedMaskItem)currentHeld, time);
                     localPlayer.StartCoroutine(possessPlayerDelayed);
                 }
             }
-            // Else if the player holds a mask try to switch slots to the mask held for the longest time
-            else if (longestHeldIndex != -1 && nextTimeToSwitchSlot <= 0f)
+            // Else if the player holds a mask try to switch slots to the closest mask
+            else if (timeHeldByPlayer > Plugin.timeToStartSwitchingSlots.Value && 
+                     nextTimeToSwitchSlot <= 0f)
             {
                 Plugin.Log.LogInfo("Player is not holding a mask, switching to longest held mask");
+
                 int numOfSlots = localPlayer.ItemSlots.Length;
-                int halfOfSlots = numOfSlots / 2;
-                int currentIndex = localPlayer.currentItemSlot;
-                int switchToIndex;
-                if (currentIndex < longestHeldIndex)
+                GrabbableObject[] inventory = localPlayer.ItemSlots;
+                int currIndex = localPlayer.currentItemSlot;
+                int switchToIndex = -1;
+                for (int i = 1; i < numOfSlots / 2; i++)
                 {
-                    if ((currentIndex - longestHeldIndex) % numOfSlots >= halfOfSlots)
-                        switchToIndex = localPlayer.NextItemSlot(true);
-                    else
-                        switchToIndex = localPlayer.NextItemSlot(true);
-                }
-                else
-                {
-                    if ((currentIndex - longestHeldIndex) % numOfSlots < halfOfSlots)
-                        switchToIndex = localPlayer.NextItemSlot(true);
-                    else
-                        switchToIndex = localPlayer.NextItemSlot(true);
+                    GrabbableObject item = inventory[(currIndex + i) % numOfSlots];
+                    if (item != null && item.GetType() == typeof(HauntedMaskItem))
+                    {
+                        switchToIndex = (currIndex + 1) % numOfSlots;
+                        break;
+                    }
+                    item = inventory[(currIndex - i) % numOfSlots];
+                    if (item != null && item.GetType() == typeof(HauntedMaskItem))
+                    {
+                        switchToIndex = (currIndex - 1) % numOfSlots;
+                        break;
+                    }
                 }
 
-                Plugin.Log.LogInfo("Switching to inventory slot " + switchToIndex);
-                if (localPlayer.ItemSlots[localPlayer.currentItemSlot].itemProperties.twoHanded)
-                    localPlayer.ItemSlots[localPlayer.currentItemSlot].DiscardItemOnClient();
-                if (Random.Range(0f, 1f) < 0.25f)
-                    localPlayer.itemAudio.PlayOneShot(slotSwitchSounds[Random.Range(0, slotSwitchSounds.Length)], localPlayer.itemAudio.volume * 0.25f);
-                localPlayer.SwitchToItemSlot(switchToIndex);
-
-                nextTimeToSwitchSlot = Random.Range(7f, 15f);
+                Plugin.Log.LogInfo($"Switching to inventory slot {switchToIndex}, current item is two handed: {currentHeld?.itemProperties.twoHanded}");
+                bool currentIsTwoHanded = currentHeld != null && currentHeld.itemProperties.twoHanded;
+                localPlayer.StartCoroutine(
+                    SwitchSlot(
+                        switchToIndex, 
+                        currentIsTwoHanded
+                        )
+                    );
+                
+                nextTimeToSwitchSlot = Random.Range(minTimeToSwitchSlots, maxTimeToSwitchSlots);
+                minTimeToSwitchSlots -= (minTimeToSwitchSlots > minSwitchingSlotTime ? deltaTimeToSwitchSlots : 0f);
+                maxTimeToSwitchSlots -= (maxTimeToSwitchSlots > minSwitchingSlotTime ? deltaTimeToSwitchSlots : 0f);
                 Plugin.Log.LogInfo("Next switch in " + nextTimeToSwitchSlot + " seconds");
             }
         }
@@ -143,6 +189,18 @@ namespace PossessedMask.Patches
             yield return new WaitForSeconds(time);
             item.ItemActivate(false, false);
             item.CancelAttachToPlayerOnLocalClient();
+        }
+
+        private static IEnumerator SwitchSlot(int slot, bool currentIsTwoHanded)
+        {
+            if (Plugin.twoHandedItemBehaviour.Value)
+            {
+                if (currentIsTwoHanded)
+                    yield return localPlayer.StartCoroutine(localPlayer.waitToEndOfFrameToDiscard());
+                if (Random.Range(0f, 1f) < 0.25f)
+                    localPlayer.itemAudio.PlayOneShot(slotSwitchSounds[Random.Range(0, slotSwitchSounds.Length)], localPlayer.itemAudio.volume * 0.25f);
+                localPlayer.SwitchToItemSlot(slot);
+            }
         }
     }
 }
--- PossessedMask/Plugin.cs
+++ PossessedMask/Plugin.cs
@@ -1,4 +1,5 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
 
 namespace PossessedMask
@@ -16,6 +17,29 @@ namespace PossessedMask
 
         internal static BepInEx.Logging.ManualLogSource Log;
 
+        // config
+        
+        internal static ConfigEntry<float> timeToStartSwitchingSlots; // after this much time with mask in inventory start switching slots
+        
+        internal static ConfigEntry<float> timeToStartPossession; // after this much time with mask in inventory start possessions
+
+        internal static ConfigEntry<bool> twoHandedItemBehaviour; // if true, 2 handed items will be dropped when switching slots
+        
+        internal static ConfigEntry<float> minTimeToSwitchSlots; // minimum time between switching slots
+        internal static ConfigEntry<float> maxTimeToSwitchSlots; // maximum time between switching slots
+        internal static ConfigEntry<float> deltaTimeToSwitchSlots; // time to subtract from min and max each time a switch happens
+        internal static ConfigEntry<float> minSwitchingSlotTime; // the number at which subtracting from min and max stops
+        
+        internal static ConfigEntry<float> minTimeToPossess; // minimum time between possessions
+        internal static ConfigEntry<float> maxTimeToPossess; // maximum time between possessions
+        internal static ConfigEntry<float> deltaTimeToPossess; // time to subtract from min and max each time a possession happens
+        internal static ConfigEntry<float> minPossessingTime; // the number at which subtracting from min and max stops
+        
+        internal static ConfigEntry<float> minTimeToPossessPlayer; // minimum time of actual possession
+        internal static ConfigEntry<float> maxTimeToPossessPlayer; // maximum time of actual possession
+        internal static ConfigEntry<float> deltaTimeToPossessPlayer; // time to add to min and max each time a possession happens
+        internal static ConfigEntry<float> maxPossessingPlayerTime; // the number at which adding to min and max stops
+        
         private void Awake()
         {
             Log = this.Logger;
@@ -24,6 +48,29 @@ namespace PossessedMask
             if (Instance == null)
                 Instance = this;
 
+            // config
+            
+            timeToStartSwitchingSlots = Config.Bind("General", "TimeToStartSwitchingSlots", 5f, "After this much time with mask in inventory start switching slots");
+            
+            timeToStartPossession = Config.Bind("General", "TimeToStartPossession", 10f, "After this much time with mask in inventory start possessions");
+            
+            twoHandedItemBehaviour = Config.Bind("General", "TwoHandedItemBehaviour", true, "If true, 2 handed items will be dropped when switching slots");
+            
+            minTimeToSwitchSlots = Config.Bind("Time To Switch Slots", "MinTimeToSwitchSlots", 12f, "Minimum time between switching slots");
+            maxTimeToSwitchSlots = Config.Bind("Time To Switch Slots", "MaxTimeToSwitchSlots", 20f, "Maximum time between switching slots");
+            deltaTimeToSwitchSlots = Config.Bind("Time To Switch Slots", "DeltaTimeToSwitchSlots", 0.5f, "Time to subtract from min and max each time a switch happens");
+            minSwitchingSlotTime = Config.Bind("Time To Switch Slots", "MinSwitchingSlotTime", 3f, "The number at which subtracting from min and max stops");
+            
+            minTimeToPossess = Config.Bind("Time To Possess", "MinTimeToPossess", 20f, "Minimum time between possessions");
+            maxTimeToPossess = Config.Bind("Time To Possess", "MaxTimeToPossess", 30f, "Maximum time between possessions");
+            deltaTimeToPossess = Config.Bind("Time To Possess", "DeltaTimeToPossess", 1f, "Time to subtract from min and max each time a possession happens");
+            minPossessingTime = Config.Bind("Time To Possess", "MinPossessingTime", 10f, "The number at which subtracting from min and max stops");
+            
+            minTimeToPossessPlayer = Config.Bind("Item Activation Time", "MinTimeToPossessPlayer", 2f, "Minimum time of actual possession");
+            maxTimeToPossessPlayer = Config.Bind("Item Activation Time", "MaxTimeToPossessPlayer", 4f, "Maximum time of actual possession");
+            deltaTimeToPossessPlayer = Config.Bind("Item Activation Time", "DeltaTimeToPossessPlayer", 1f, "Time to add to min and max each time a possession happens");
+            maxPossessingPlayerTime = Config.Bind("Item Activation Time", "MaxPossessingPlayerTime", 9f, "The number at which adding to min and max stops");
+            
             harmony.PatchAll();
 
             Log.LogInfo($"'{NAME}' loaded!");
