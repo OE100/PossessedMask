6732605d0879cb1b2bd7380e192d48d16e820867
OE100 <omer.er10@gmail.com>
2024-01-30 16:00:26 +0200

    no need to do the same work the game does already

---
+++ PossessedMasks/patches/RoundManagerPatch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace PossessedMasks.patches;
+
+[HarmonyPatch(typeof(RoundManager))]
+public class RoundManagerPatch
+{
+    [HarmonyPatch(nameof(RoundManager.FinishGeneratingLevel)), HarmonyPostfix]
+    private static void FinishGeneratingLevelPostfix(RoundManager __instance)
+    {
+        Utils.InsideAINodes = __instance.insideAINodes;
+        Utils.OutsideAINodes = __instance.outsideAINodes;
+    }
+}
