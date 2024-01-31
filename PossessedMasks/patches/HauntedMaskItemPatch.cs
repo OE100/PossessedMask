cdcb57473fb9026d857a5d0c500e8589ec8c4153
OE100 <omer.er10@gmail.com>
2024-01-31 15:55:52 +0200

    changed the patch that adds the mask controller

--- PossessedMasks/patches/HauntedMaskItemPatch.cs
+++ PossessedMasks/patches/HauntedMaskItemPatch.cs
@@ -1,24 +1,17 @@
 ﻿using HarmonyLib;
-using PossessedMasks.mono;
+using PossessedMasks.machines.impl.mask;
 
 namespace PossessedMasks.patches;
 
 [HarmonyPatch(typeof(HauntedMaskItem))]
 public class HauntedMaskItemPatch
 {
-    [HarmonyPatch(nameof(HauntedMaskItem.DiscardItem)), HarmonyPrefix]
-    private static void DiscardItemPrefix(HauntedMaskItem __instance)
+    [HarmonyPatch(nameof(HauntedMaskItem.Start)), HarmonyPostfix]
+    private static void StartPostfix(HauntedMaskItem __instance)
     {
         if (!Utils.HostCheck) return;
-        if (!Utils.InLevel) return;
-        if (SharedConfig.LurkingMechanicEnabled)
-        {
-            var crawlingComponent = __instance.gameObject.GetComponent<CrawlingComponent>();
-            if (crawlingComponent)
-                crawlingComponent.Inside = __instance.playerHeldBy.isInsideFactory;
-            else
-                __instance.gameObject.AddComponent<CrawlingComponent>().Inside =
-                    __instance.playerHeldBy.isInsideFactory;
-        }
+        if (!SharedConfig.LurkingMechanicEnabled) return;
+        
+        __instance.gameObject.AddComponent<MaskStateManager>();
     }
 }
