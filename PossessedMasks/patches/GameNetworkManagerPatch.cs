8b580145f9a87ca427e1346bf7d9442e81f6f518
OE100 <omer.er10@gmail.com>
2024-02-01 15:01:12 +0200

    removed delayed player action replacement in favour of player patching

--- PossessedMasks/patches/GameNetworkManagerPatch.cs
+++ PossessedMasks/patches/GameNetworkManagerPatch.cs
@@ -14,42 +14,5 @@ public class GameNetworkManagerPatch
     {
         // register network prefab
         Plugin.NetworkPrefabs.ForEach(prefab => NetworkManager.Singleton.AddNetworkPrefab(prefab));
-        
-        // start coroutine to modify player
-        __instance.StartCoroutine(DelayedModifyPlayer());
-    }
-
-    private static IEnumerator DelayedModifyPlayer()
-    {
-        yield return new WaitUntil(() => PossessedBehaviour.Instance);
-        yield return new WaitUntil(() => StartOfRound.Instance);
-        yield return new WaitUntil(() => IngamePlayerSettings.Instance);
-        yield return new WaitUntil(() => StartOfRound.Instance.localPlayerController);
-        yield return new WaitUntil(() => IngamePlayerSettings.Instance.playerInput);
-        
-        try
-        {
-            IngamePlayerSettings.Instance.playerInput.actions.FindAction("Discard").performed -=
-                StartOfRound.Instance.localPlayerController.Discard_performed;
-            IngamePlayerSettings.Instance.playerInput.actions.FindAction("Discard").performed +=
-                PossessedBehaviour.Discard_with_check_performed;
-            Plugin.Log.LogMessage("Discard override complete");
-        }
-        catch (Exception)
-        {
-            Plugin.Log.LogMessage("Didn't override discard");
-        }
-        
-        try
-        {
-            IngamePlayerSettings.Instance.playerInput.actions.FindAction("Interact").performed -=
-                StartOfRound.Instance.localPlayerController.Interact_performed;
-            IngamePlayerSettings.Instance.playerInput.actions.FindAction("Interact").performed +=
-                PossessedBehaviour.Interact_with_check_performed;
-            Plugin.Log.LogMessage("Interact override complete");
-        } catch (Exception)
-        {
-            Plugin.Log.LogMessage("Didn't override interact");
-        }
     }
 }
---
+++ PossessedMasks/patches/PlayerControllerBPatch.cs
@@ -0,0 +1,29 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+
+namespace PossessedMasks.patches;
+
+[HarmonyPatch(typeof(PlayerControllerB))]
+public class PlayerControllerBPatch
+{
+    [HarmonyPatch(nameof(PlayerControllerB.Discard_performed)), HarmonyPrefix, HarmonyPriority(Priority.First)]
+    private static bool Discard_performedPrefix(PlayerControllerB __instance)
+    {
+        var localPlayer = StartOfRound.Instance.localPlayerController;
+        var heldObject = localPlayer.currentlyHeldObjectServer;
+        if (Utils.InLevel && heldObject is HauntedMaskItem && Utils.ItemCount(localPlayer) < SharedConfig.ItemCount) 
+            return false;
+        return true;
+    }
+    
+    [HarmonyPatch(nameof(PlayerControllerB.Interact_performed)), HarmonyPrefix, HarmonyPriority(Priority.First)]
+    private static bool Interact_performedPrefix(PlayerControllerB __instance)
+    {
+        var localPlayer = StartOfRound.Instance.localPlayerController;
+        var heldObject = localPlayer.currentlyHeldObjectServer;
+        if (Utils.InLevel && heldObject is HauntedMaskItem && 
+            (localPlayer.activatingItem || Utils.ItemCount(localPlayer) < SharedConfig.ItemCount)) 
+            return false;
+        return true;
+    }
+}
